4b4d4c06dc541abdada9562b898a69c9fc1feff9
ootes <mark.ootes@gmail.com>
2013-03-07 00:13:10 +0100

    add observer

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ Meteor.methods({
 });
 
 Meteor.call('getUserId', function(error, Id){
-
 	var user = Meteor.users.findOne({_id: Id});
+
 	var hasCharacter = function(){
 		return Characters.findOne({owner: Id});
 	};
@@ -49,7 +49,28 @@ Meteor.call('getUserId', function(error, Id){
 		});
 
 	}
-
 });
+
+
+var query = Characters.find();
+var handle = query.observeChanges({
+	changed: function(id, fields){
+		console.log(id, fields);
+  	}	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
