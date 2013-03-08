a7bf519b5234f08d08bad993ef00f518299faf71
Imro Breur <imrobreur@Imros-MacBook-Pro.local>
2013-03-08 11:08:57 +0100

    swipe events

--- client/swipe.js
+++ client/swipe.js
@@ -1,9 +1,36 @@
-$(document).on('swipeleft', function(event){
-	if (!move) return;
-	move = false;
+// $(document).on('swipeleft', function(event){
+// 	if (!move) return;
+// 	move = false;
  	
-		moveCharacter([-30, 0], "left");
+// 		moveCharacter([-30, 0], "left");
+// });
 
+Template.gamecanvas.swipeme = function () {
+	Meteor.defer(function() {
+	    var $sw = $('#gamecanvas');
+	    $sw.hammer().on('swipe', function (event) {
+	    	event.preventDefault();
+	  //     if (!move) return;
+			// move = false;
+	 		console.log(event);	
+
+	 		if (event.gesture.direction === "left") {
+	 			moveCharacter([-30, 0], "left");
+	 		}
+	 		if (event.gesture.direction === "up") {
+	 			moveCharacter([0, -30], "up");
+	 		}
+	 		if (event.gesture.direction === "right") {
+	 			moveCharacter([30, 0], "right");
+	 		}
+	 		if (event.gesture.direction === "down") {
+	 			moveCharacter([0, 30], "down");
+	 		};
+			// moveCharacter([-30, 0], "left");
+	  //     	console.log("Type: " + event.type + ", Fingers: " + event.touches.length + ", Direction: " + event.direction + "<br/>");
+	    });
+	});
+};
 
 	// if(e.keyCode == 38) {
 	// 	// move up
@@ -17,7 +44,7 @@ $(document).on('swipeleft', function(event){
 	// 	// down
 	// 	moveCharacter([0, 30], "down");
 	// }
-});
+
 
 
 	$(window).on('touchmove', function (ev) { 
